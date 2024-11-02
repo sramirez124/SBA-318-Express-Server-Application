74cf4a2bc49a7d37361b38a9d7907c122fa5a4be
sramirez124 <forwardemails124@gmail.com>
2024-11-02 15:34:57 -0500

    Added in users

---
+++ data/users.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+        id: 1,
+        name: "Gandalf the Grey",
+        role: "wizard",
+        username: "CantPassMe2",
+        password: "iHaveNoMemory",
+        email: "firedude@hobbiton.com"
+    },
+    {
+        id: 2,
+        name: "Frodo Baggins",
+        role: "Ring Bearer",
+        username: "Ringbearer1",
+        password: "ringbearer",
+        email: "ringbearer1@hobbiton.com"
+    },
+    {
+        id: 3,
+        name: "Samwise Gamgee",
+        role: "Actual Hero",
+        username: "StewMaster124",
+        password: "MrFrodo123",
+        email: "gardener@hobbiton.com"
+    },
+    {
+        id: 4,
+        name: "Aragorn",
+        role: "King of Gondor",
+        username: "Strider14",
+        password: "Gondor",
+        email: "aragorn@hobbiton.com"
+    },
+    {
+        id: 5,
+        name: "Legolas",
+        role: "Archer",
+        username: "42KillsConfirmed",
+        password: "HobbitsAtIsengard",
+        email: "legolas@hobbiton.com"
+    },
+    {
+        id: 6,
+        name: "Gimli",
+        role: "Dwarf",
+        username: "NeverThrownOnce43",
+        password: "MithrilRocks",
+        email: "gimli@hobbiton.com"
+    }
+
+];
