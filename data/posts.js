9c1e69f75cd99f03a41d54cf748a04eb33283cea
sramirez124 <forwardemails124@gmail.com>
2024-11-05 22:40:24 -0600

    post made

---
+++ data/posts.js
@@ -0,0 +1,60 @@
+const posts = [
+    {
+      id: 1,
+      userId: 1, // Gandalf
+      title: "Mines of Moria? I can't remember",
+      content:
+        "I geuinely cannot remember these caves, I got no idea where I am going #lol",
+    },
+    {
+      id: 2,
+      userId: 1, // Gandalf
+      title: "All the exp belongs to me",
+      content:
+      "Told my party I wouldn't let the Balrog pass and for them to fly, but I just wanted to get the loot and exp from the boss",
+    },
+    {
+      id: 3,
+      userId: 2,  // Frodo
+      title: "Bilbo left and all I got was his ring",
+      content:
+        "I spend all this time helping set up the party and the dude dipped out, he left me this cool ring though",
+    },
+    {
+      id: 4,
+      userId: 2, // Frodo
+      title: "So I was stabbed",
+      content:
+        "Turns out the ring was not cool and it belong to some dude, his goons ended up stabbing me trying to take it back but after I got stabbed I woke up in Rivendell",
+    },
+    {
+      id: 5,
+      userId: 3, // Samwise
+      title: "Brb going on a short trip",
+      content:
+        "Gandalf politely asked me to watch over Frodo as he makes a run to Bree, should be a quick one",
+    },
+    {
+      id: 6,
+      userId: 4, // Aragorn
+      title: "Never listen to Gandalf again",
+      content:
+        "Went from having to meet two hobbits to rushing one hobbit to the Rivendell and watching over three other hobbits",
+    },
+    {
+      id: 7,
+      userId: 5, // Legolas
+      title: "This dwarf is a chill dude",
+      content:
+        "He was super annoying at first but hes a chill dude, I still won the competition though",
+    },
+    {
+      id: 8,
+      userId: 6, // Gimli
+      title: "Short king can run short distances",
+      content:
+        "They got me running cross country when I'm a king at short distances.",
+    },
+  ];
+  
+  module.exports = posts;
